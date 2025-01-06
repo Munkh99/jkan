58bb136d8d647588527f79b286ff231ca935e318
munkh99 <munkh99ro@gmail.com>
2025-01-06 11:31:24 +0100

    year fiterrrr

--- scripts/src/components/year-filter.js
+++ scripts/src/components/year-filter.js
@@ -1,38 +1,33 @@
-import $ from 'jquery'
-import {chain, pick, omit, filter, defaults} from 'lodash'
-
-import TmplListGroupItem from '../templates/list-group-item'
-import {setContent, slugify, createDatasetFilters, collapseListGroup} from '../util'
-
 export default class {
-  constructor (opts) {
-    const years = this._yearsWithCount(opts.datasets, opts.params)
-    const yearsMarkup = years.map(TmplListGroupItem)
-    setContent(opts.el, yearsMarkup)
-    collapseListGroup(opts.el)
+  constructor(opts) {
+    const years = this._yearsWithCount(opts.datasets, opts.params);
+    const yearsMarkup = years.map(TmplListGroupItem);
+    setContent(opts.el, yearsMarkup);
+    collapseListGroup(opts.el);
     console.log('init year2 filter');
-    console.log(years)
+    console.log(years);
   }
 
-  _yearsWithCount (datasets, params) {
+  _yearsWithCount(datasets, params) {
     return chain(datasets)
-      .groupBy('start_date') // Group datasets by year
-      .map((datasetsInYear, year) => {
-        const filters = createDatasetFilters(pick(params, ['category']))
-        const filteredDatasets = filter(datasetsInYear, filters)
-        const yearSlug = slugify(year)
-        const selected = params.year && params.year === yearSlug
-        const itemParams = selected ? omit(params, 'start_date') : defaults({year: yearSlug}, params)
+      .groupBy('start_date') // Group datasets by full start_date
+      .map((datasetsInYear, startDate) => {
+        const filters = createDatasetFilters(pick(params, ['category']));
+        const filteredDatasets = filter(datasetsInYear, filters);
+        const startDateSlug = slugify(startDate); // Use full date as slug
+        const selected = params.start_date && params.start_date === startDateSlug;
+        const itemParams = selected
+          ? omit(params, 'start_date')
+          : defaults({ start_date: startDateSlug }, params);
         return {
-          title: year, // display the year
+          title: startDate, // Display full date
           url: '?' + $.param(itemParams),
           count: filteredDatasets.length,
           unfilteredCount: datasetsInYear.length,
-          selected: selected
-        }
+          selected: selected,
+        };
       })
-      .orderBy('unfilteredCount', 'desc')
-      .value()
+      .orderBy('unfilteredCount', 'desc') // Order by count
+      .value();
   }
 }
-
